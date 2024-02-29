f317ac7cd04d32ae454e5270b5533d5ca354aa7d
PAVITRA.A <121439141+PAVI57@users.noreply.github.com>
2024-02-29 21:30:43 +0530

    Add files via upload

---
+++ Say_Yes/index.js
@@ -0,0 +1,30 @@
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
+
+yesBtn.addEventListener('click', () => {
+    question.innerHTML = 'I Love You Tooo🖤';
+
+    // Create hearts and add them to the wrapper
+    const heartsContainer = document.createElement('div');
+    heartsContainer.classList.add('hearts-container');
+    for (let i = 0; i < 10; i++) {
+        const heart = document.createElement('div');
+        heart.classList.add('heart');
+        heart.style.left = Math.random() * 100 + '%';
+        heart.style.animationDelay = Math.random() * 2 + 's';
+        heartsContainer.appendChild(heart);
+    }
+    wrapper.appendChild(heartsContainer);
+});
+
+
+noBtn.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+    noBtn.style.left = i + 'px';
+    noBtn.style.top = j + 'px';
+});
